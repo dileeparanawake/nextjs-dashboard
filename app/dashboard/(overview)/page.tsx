c346956f6e3738ef6b0605835cf51f9061c0bc8b
dileeparanawake <dileeparanawake@icloud.com>
2025-06-15 16:08:57 +0100

    add metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -17,6 +17,11 @@ import {
   CardsSkeleton,
 } from "@/app/ui/skeletons";
 import { Cardo } from "next/font/google";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
 
 export default async function Page() {
   // const revenue = await fetchRevenue();
